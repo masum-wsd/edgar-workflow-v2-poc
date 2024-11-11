1b4a3a3c0a1431448b69092e79fb28b75fadbca2
Masum <abdullah.almasum@wsd.com>
2024-11-11 17:57:56 +0600

    feat: for .readthedocs.yaml file -> docs/conf.py added

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+﻿# -- Project information -----------------------------------------------------
+project = 'Your Project Name'
+author = 'Your Name or Team'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # If you use Google or NumPy style docstrings
+]
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'  # or another theme like 'sphinx_rtd_theme'
